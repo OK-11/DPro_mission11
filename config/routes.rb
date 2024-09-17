9d29b5608fd346d609b1b786480057d24971cc7d
尾澤 公佑 <koushuke06@gmail.com>
2024-09-17 18:41:38 +0900

    はじめてのコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+
+  
+# or require 'sidekiq/pro/web'
+# or require 'sidekiq-ent/web'
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
+
 end
